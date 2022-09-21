031c48b1fdcdc7c9367a37d8d4d04f5880930568
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-09-21 21:37:17 +0700

    Add project files.

---
+++ WindowManageCategories.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+
+namespace ManageCategoriesApp
+{
+    /// <summary>
+    /// Interaction logic for WindowManageCategories.xaml
+    /// </summary>
+    public partial class WindowManageCategories : Window
+    {
+        public WindowManageCategories()
+        {
+            InitializeComponent();
+        }
+        ManageCategories categories = new ManageCategories();
+
+        private void Window_Load(object sender, RoutedEventArgs e) => LoadCategories();
+
+        private void LoadCategories()
+        {
+            lvCategories.ItemsSource = categories.GetCategories();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var category = new Category { CategoryName = txtCategoryName.Text };
+                categories.InsertCategory(category);
+                LoadCategories();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Insert Category");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var category = new Category
+                {
+                    CategoryID = int.Parse(txtCategoryID.Text),
+                    CategoryName = txtCategoryName.Text,
+                };
+                categories.UpdateCategory(category);
+                LoadCategories();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Update Category");
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var Category = new Category
+                {
+                    CategoryID = int.Parse(txtCategoryID.Text)
+                };
+                categories.DeleteCategory(Category);
+                LoadCategories();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Delete Category");
+            }
+        }
+    }
+}
